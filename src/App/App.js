097f4c31c162e9fa3248f80ce4b4d56831d5d630
franciscofunes <ffunes90@gmail.com>
2021-06-15 20:12:37 -0300

    creador de memes portexe tutorial

---
+++ src/App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Meme } from '../Meme/Meme';
+import { Switch, Route } from 'react-router-dom';
+import { MemeGenerated } from '../MemeGenerated/MemeGenerated';
+//import styles from './styles.module.css';
+
+export const App = () => {
+	return (
+		<div>
+			<h1>Creador De Memes</h1>
+			<Switch>
+				<Route exact path='/'>
+					<Meme />
+				</Route>
+				<Route path='/memecreado'>
+					<MemeGenerated />
+				</Route>
+			</Switch>
+		</div>
+	);
+};
