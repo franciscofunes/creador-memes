c932694fd1c12119ae656436c52bcaf1c6a12262
franciscofunes <48732867+franciscofunes@users.noreply.github.com>
2022-08-31 20:03:39 -0300

    style fixes

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Meme } from '../Meme/Meme';
 import { Switch, Route } from 'react-router-dom';
 import { MemeGenerated } from '../MemeGenerated/MemeGenerated';
-//import styles from './styles.module.css';
+import styles from './styles.module.css';
 
 export const App = () => {
 	return (
-		<div>
+		<div className={styles.container}>
 			<h1>Creador De Memes</h1>
 			<Switch>
 				<Route exact path='/'>
